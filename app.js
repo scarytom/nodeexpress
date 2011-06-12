7b2c84d97596c9a4496f0d2bb68702a9b820ffff
scarytom <t.denley@cantab.net>
2011-06-12 16:01:40 +0100

    tidy up generated app.js

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-
-var app = module.exports = express.createServer();
-
-// Configuration
+"user strict";
+var express = require("express"),
+    twitter = require("./twitter"),
+    app = module.exports = express.createServer();
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set("views", __dirname + "/views");
+  app.set("view engine", "jade");
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + "/public"));
 });
 
-app.configure('development', function(){
+app.configure("development", function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-app.configure('production', function(){
+app.configure("production", function(){
   app.use(express.errorHandler()); 
 });
 
 // Routes
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
+app.get("/", function(req, res){
+  res.render("index", {
+    title: "Express"
   });
 });
 
